490e1a1ea05f1b2db24a8cdb1a0ca8751b873601
Ken Burres <ken@khbtechservices.com>
2026-03-02 11:10:03 -0500

    Update headings

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,23 +9,24 @@ fi
 # --------------------------------
 INFO='\033[0;30;106m'
 SUCCESS='\033[0;30;42m'
+BOLD_DEFAULT='\033[1m;'
 RESET='\033[0m'
 
 set -e
 
 # Update System
-echo -e " ${INFO} UPDATE ${RESET} Updating & Upgrading System"
+echo -e " ${INFO} UPDATE ${RESET} ${BOLD_DEFAULT}Updating & Upgrading System${RESET}"
 apt update &&  apt upgrade -y
 
 # Install SSH
-echo -e " ${INFO} SSH ${RESET} Installing SSH"
+echo -e " ${INFO} SSH ${RESET} ${BOLD_DEFAULT}Installing SSH${RESET}"
 apt install -y openssh-server
-echo -e " ${INFO} SSH ${RESET} Enabling SSH"
+echo -e " ${INFO} SSH ${RESET} ${BOLD_DEFAULT}Enabling SSH${RESET}"
 systemctl enable ssh
 systemctl start ssh
 
 # Install vim
-echo -e " ${INFO} VIM ${RESET} Installing VIM"
+echo -e " ${INFO} VIM ${RESET} ${BOLD_DEFAULT}Installing VIM${RESET}"
 apt install -y vim
 
-echo -e " ${SUCCESS} DONE! ${RESET} Done!"
+echo -e " ${SUCCESS} DONE! ${RESET} ${BOLD_DEFAULT}Done!${RESET}"
