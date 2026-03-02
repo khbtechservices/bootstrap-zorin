4c4cd32ea9cee942edbb0d160fb0d666a12ad227
Ken Burres <ken@khbtechservices.com>
2026-03-02 10:53:36 -0500

    Fix coloring issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,27 +7,25 @@ fi
 # --------------------------------
 # Setup Variables
 # --------------------------------
-BGCYAN='\033[0;106m'
-FGCYAN='\033[0;96m'
-FGBLACK='\033[0;30m'
-BGGREEN='\033[0;42m'
+INFO='\033[0;30;106m'
+SUCCESS='\033[0;30;42m'
 RESET='\033[0m'
 
 set -e
 
 # Update System
-echo -e " ${BGCYAN}${FGBLACK} UPDATE ${RESET} Updating & Upgrading System"
+echo -e " ${INFO} UPDATE ${RESET} Updating & Upgrading System"
 apt update &&  apt upgrade -y
 
 # Install SSH
-echo -e " ${BGCYAN}${FGBLACK} SSH ${RESET} Installing SSH"
+echo -e " ${INFO} SSH ${RESET} Installing SSH"
 apt install -y openssh-server
-echo -e " ${BGCYAN}${FGBLACK} SSH ${RESET} Enabling SSH"
+echo -e " ${INFO} SSH ${RESET} Enabling SSH"
 systemctl enable ssh
 systemctl start ssh
 
 # Install vim
-echo -e " ${BGCYAN}${FGBLACK} VIM ${RESET} Installing VIM"
+echo -e " ${INFO} VIM ${RESET} Installing VIM"
 apt install -y vim
 
-echo -e " ${BGGREEN}${FGBLACK} DONE! ${RESET} Done!"
+echo -e " ${ISUCCESSO} DONE! ${RESET} Done!"
