a42552667e229c6788797f30f4afa53fb72a10e3
Ken Burres <ken@khbtechservices.com>
2026-03-02 10:24:08 -0500

    Add flare

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,30 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+# --------------------------------
+# Setup Variables
+# --------------------------------
+BGCYAN='\033[0;106m'
+FGCYAN='\033[0;96m'
+FGBLACK='\033[0;30m'
+BGGREEN='\033[0;42m'
+RESET='\033[0m'
+
 set -e
 
 # Update System
+echo " ${BGCYAN}${FGBLACK} UPDATE ${RESET} Updating & Upgrading System"
 apt update &&  apt upgrade -y
 
 # Install SSH
+echo " ${BGCYAN}${FGBLACK} SSH ${RESET} Installing SSH"
 apt install -y openssh-server
+echo " ${BGCYAN}${FGBLACK} SSH ${RESET} Enabling SSH"
 systemctl enable ssh
 systemctl start ssh
 
 # Install vim
+echo " ${BGCYAN}${FGBLACK} VIM ${RESET} Installing VIM"
 apt install -y vim
+
+echo " ${BGGREEN}${FGBLACK} DONE! ${RESET} Done!"
